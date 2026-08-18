4e89bac939894382723ed1e997e27577afda0bac
yelhamdi <yassin890890.890@gmail.com>
2026-08-18 15:12:04 +0100

    add backtick

--- sh/src/parser.rs
+++ sh/src/parser.rs
@@ -7,29 +7,45 @@ pub fn tokenize(input: &str) -> TokenizeState {
     let mut tokens = Vec::new();
     let mut current = String::new();
     let mut chars = input.chars().peekable();
+
     let mut in_single = false;
     let mut in_double = false;
+    let mut in_backtick = false;
 
     while let Some(c) = chars.next() {
         match c {
-            '\'' if !in_double => {
+            // Single quote
+            '\'' if !in_double && !in_backtick => {
                 in_single = !in_single;
             }
 
-            '"' if !in_single => {
+            // Double quote
+            '"' if !in_single && !in_backtick => {
                 in_double = !in_double;
             }
 
-            '#' if !in_single && !in_double => {
+            // Backtick
+            '`' if !in_single && !in_double => {
+                in_backtick = !in_backtick;
+            }
+
+            // Comment
+            '#' if !in_single && !in_double && !in_backtick => {
                 break;
             }
 
-            c if c.is_whitespace() && !in_single && !in_double => {
+            // Whitespace separates tokens only outside quotes
+            c if c.is_whitespace()
+                && !in_single
+                && !in_double
+                && !in_backtick =>
+            {
                 if !current.is_empty() {
                     tokens.push(std::mem::take(&mut current));
                 }
             }
 
+            // Backslash escapes the next character
             '\\' if !in_single => {
                 if let Some(next) = chars.next() {
                     current.push(next);
@@ -38,13 +54,15 @@ pub fn tokenize(input: &str) -> TokenizeState {
                 }
             }
 
+            // Normal character
             _ => {
                 current.push(c);
             }
         }
     }
 
-    if in_single || in_double {
+    // An unclosed quote/backtick means the command is incomplete
+    if in_single || in_double || in_backtick {
         return TokenizeState::Incomplete;
     }
 
