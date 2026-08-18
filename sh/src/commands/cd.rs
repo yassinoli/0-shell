15a5e4a6494a6c11f1b5671c496f8e9d7d55676e
wkhlifi <wkhlifi654@gmail.com>
2026-08-18 13:01:32 +0100

    cd -- -

--- sh/src/commands/cd.rs
+++ sh/src/commands/cd.rs
@@ -3,12 +3,22 @@ use std::env;
 use std::path::Path;
 
 pub fn run(args: &[String]) -> Result<Status, String> {
-    let target = match args.len() {
+    let mut newargs: Vec<String> = Vec::new();
+    for c in args{
+        if c=="--"{
+        continue;
+        }else{
+            newargs.push(c.clone())
+        }
+    }
+    let target = match newargs.len() {
         0 => env::var("HOME").unwrap_or_else(|_| "/".to_string()),
-        1 if args[0] == "-" => take_previous_dir()
+       1 if args[0] == "-" => take_previous_dir()
             .map(|path| path.to_string_lossy().into_owned())
-            .ok_or_else(|| "cd: OLDPWD not set".to_string())?,
-        1 => expand_tilde(&args[0]),
+            .unwrap_or_else(|| {
+                env::var("HOME").unwrap_or_else(|_| "/".to_string())
+            }),
+        1 => expand_tilde(&newargs[0]),
         _ => return Err("cd: too many arguments".to_string()),
     };
 
@@ -17,7 +27,7 @@ pub fn run(args: &[String]) -> Result<Status, String> {
         .map_err(|e| format!("cd: {}: {}", target, e))?;
     set_previous_dir(current_dir);
 
-    if args.len() == 1 && args[0] == "-" {
+    if newargs.len() == 1 && newargs[0] == "-" {
         println!("{}", target);
     }
 
